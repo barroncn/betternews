8065557e916b0fd1f84d0bb6e1e5b0894557fb52
Caitlin Barron <barroncn@gmail.com>
2018-01-19 18:50:02 +0000

    modified server.js for mongo and mongo debug

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const path = require("path");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 const PORT = process.env.PORT || 8081;
 const app = express();
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: false }));
+// Use express.static to serve the public folder as a static directory
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Set mongoose to leverage built in Promises && Connect to Mongo DB---------------------------
+mongoose.Promise = Promise;
+
+// If deployed, use the deployed database. Otherwise use the local myreps database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/myreps";
+mongoose.connect(MONGODB_URI);
+mongoose.set('debug', true);
+
+// A GET route for scraping the echojs website
+var routes = require("./controllers/controller.js");
+app.use("/", routes);
 
 // Send every request to the React app
 // Define any API routes before this runs
