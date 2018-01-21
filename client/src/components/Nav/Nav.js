35b9d626c46135dc682891cdde6fb6a6e7f2af61
Caitlin Barron <barroncn@gmail.com>
2018-01-21 16:07:19 +0000

    update nav

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-const Nav = props =>
-    <nav class="navbar" role="navigation" aria-label="main navigation">
-          <div class="navbar-brand is-size-3">
+const Nav = props => {
+    <div className="navbar" role="navigation" aria-label="main navigation">
+          <div className="navbar-brand is-size-3">
              My Reps 
-             <div class="is-size-5 has-text-right navbar-item navbar-end">
+             <div className="is-size-5 has-text-right navbar-item navbar-end">
                 {props.message} 
-                <a class="navbar-item" style={props.currentPage === "Home" ? "" : "display:none"}>Login</a>
-                <a class="navbar-item">{props.linktwo}</a>
+                <a className="navbar-item">{props.linkone}</a>
+                <a className="navbar-item">{props.linktwo}</a>
               </div>
           </div>
-        </nav>;
-
+        </div>;
+};
 export default Nav;
