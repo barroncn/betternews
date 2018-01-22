e089b5b4b0a5d19f8fb0963385adaf36163d418b
Caitlin Barron <barroncn@gmail.com>
2018-01-22 17:34:14 +0000

    login/register switch fix

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -5,29 +5,28 @@ import Nav from "../../components/Nav";
 const Login = props => [
 
   <Nav
-                    linkOneDisplay="Login"
-                    linkTwoDisplay="Register"
-                />,
+    linkOneDisplay="Login"
+    linkTwoDisplay="Register"
+  />,
   <div className="loginWrap">
-                <br/>
-                <div class="card">
-                  <div class="card-body">
-                    <h4 class="card-title">Login</h4>
-                  
-                <form>
-                  <div className="form-group">
-                    <label for="emailInput">Email address</label>
-                    <input type="email" className="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Enter email" />
-                    </div>
-                  <div className="form-group">
-                    <label for="passwordInput">Password</label>
-                    <input type="password" className="form-control" id="passwordInput" placeholder="Password" />
-                  </div>
-                    <button type="submit" className="btn btn-dark">Submit</button>
-                </form>
-                </div>
-                </div>
-                </div>
+    <br/>
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Login</h4>
+        <form>
+          <div className="form-group">
+            <label for="emailInput">Email address</label>
+            <input type="email" className="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Enter email" />
+          </div>
+          <div className="form-group">
+            <label for="passwordInput">Password</label>
+            <input type="password" className="form-control" id="passwordInput" placeholder="Password" />
+          </div>
+          <button type="submit" className="btn btn-dark">Submit</button>
+        </form>
+      </div>
+    </div>
+  </div>
 ];
 
 export default Login;
