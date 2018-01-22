c3fe77be78a12ea80608766e10a681139622fa0b
Caitlin Barron <barroncn@gmail.com>
2018-01-22 12:54:11 +0000

    Article Card component complete

--- client/src/components/ArticleCard/ArticleCard.js
+++ client/src/components/ArticleCard/ArticleCard.js
@@ -1,7 +1,19 @@
 import React from "react";
-import "./RepCard.css";
+import "./ArticleCard.css";
 
-const RepCard = props =>
+const ArticleCard = props =>
+    <div class="card article">
+          <div className="card-header">
+            <a href={props.url}>
+            {props.title}<span className="pull-right">Save</span>
+            </a>
+          </div>
+          <div className="card-body">
+            <blockquote className="blockquote mb-0">
+              <p>{props.summary}</p>
+              <footer className="blockquote-footer">{props.date}</footer>
+            </blockquote>
+          </div>
+        </div>;
 
-
-    export default RepCard;
+export default ArticleCard;
