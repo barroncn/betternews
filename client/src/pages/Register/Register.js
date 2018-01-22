b25a5c6bd415e943e3bcf679418338535b74589f
Caitlin Barron <barroncn@gmail.com>
2018-01-22 17:11:09 +0000

    complete Register.jspage

---
+++ client/src/pages/Register/Register.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Register.css";
+import Nav from "../../components/Nav";
+
+const Register = props => [
+
+    <Nav
+                    linkOneDisplay="Login"
+                    linkTwoDisplay="Register"
+                />,
+    <div className="loginWrap">
+                <br/>
+                <div class="card">
+                  <div class="card-body">
+                    <h4 class="card-title">Login</h4>
+                  
+                <form>
+                  <div className="form-group">
+                    <label for="emailInput">Email address</label>
+                    <input type="email" className="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Enter email" />
+                    </div>
+                  <div className="form-group">
+                    <label for="passwordInput">Password</label>
+                    <input type="password" className="form-control" id="passwordInput" placeholder="Password" />
+                  </div>
+                    <button type="submit" className="btn btn-dark">Submit</button>
+                </form>
+                </div>
+                </div>
+                </div>
+];
+
+export default Register;
