fb092634815c3955a8a6a171fccd8affcac9f7e3
Caitlin Barron <barroncn@gmail.com>
2018-01-23 16:54:33 +0000

    formatted ArticleDisplay.js

--- client/src/components/ArticleDisplay/ArticleDisplay.js
+++ client/src/components/ArticleDisplay/ArticleDisplay.js
@@ -3,10 +3,10 @@ import "./ArticleDisplay.css";
 
 const ArticleDisplay = props =>
   <div className="theArticles">
-      <h2 className="articleHeader">The Latest in Politics</h2>
-      <div className="articleDisplay">
-        {props.children}
-      </div>
-    </div>;
+    <h2 className="articleHeader">The Latest in Politics</h2>
+    <div className="articleDisplay">
+      {props.children}
+    </div>
+  </div>;
 
 export default ArticleDisplay;
