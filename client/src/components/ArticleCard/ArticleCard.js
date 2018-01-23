9a19dd33906e31920dd47aba19b7bbd1291c0b95
Caitlin Barron <barroncn@gmail.com>
2018-01-23 16:55:49 +0000

    articlecard.js formatted:

--- client/src/components/ArticleCard/ArticleCard.js
+++ client/src/components/ArticleCard/ArticleCard.js
@@ -3,18 +3,18 @@ import "./ArticleCard.css";
 
 const ArticleCard = props =>
   <div className="card article text-left">
-          <div className="card-header">
-            <h5><a href={props.url} target="_blank" className="font-weight-bold text-left">
-            {props.title}<span className="pull-right">Save</span>
-            </a></h5>
-          </div>
-          <div className="card-body">
-            <blockquote className="blockquote mb-0">
-              <p>{props.summary}</p>
-              <footer className="blockquote-footer">{props.date}</footer>
-            </blockquote>
-          </div>
-          <br/>
-        </div>;
+    <div className="card-header">
+      <h5><a href={props.url} target="_blank" className="font-weight-bold text-left">
+        {props.title}<span className="pull-right">Save</span>
+      </a></h5>
+    </div>
+    <div className="card-body">
+      <blockquote className="blockquote mb-0">
+        <p>{props.summary}</p>
+        <footer className="blockquote-footer">{props.date}</footer>
+      </blockquote>
+    </div>
+    <br/>
+  </div>;
 
 export default ArticleCard;
